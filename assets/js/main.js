5a3afb5bfa7d427a3bf6231fbf8a88cfb23618c9
Daniel <dmilano857@gmail.com>
2021-08-29 17:28:13 -0600

    logo size change

--- assets/js/main.js
+++ assets/js/main.js
@@ -423,3 +423,5 @@ setTimeout(function(){
 setTimeout(function(){
 			$('body').removeClass('stop-scrolling')
 			},3500);
+
+			$.each($('*'), function() { if ($(this).width() > $('body').width()) { console.log($(this).get(0)); } }).length;
